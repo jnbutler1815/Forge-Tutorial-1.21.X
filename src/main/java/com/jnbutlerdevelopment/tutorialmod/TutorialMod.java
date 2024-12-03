0184112b4ad378e9ad005e9ec39d9aa6be92a904
James Butler <jnbutler1815@gmail.com>
2024-12-02 20:52:09 -0600

    Added two items Alexandrite, and Raw_Alexandrite.

--- src/main/java/com/jnbutlerdevelopment/tutorialmod/TutorialMod.java
+++ src/main/java/com/jnbutlerdevelopment/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package com.jnbutlerdevelopment.tutorialmod;
 
+import com.jnbutlerdevelopment.tutorialmod.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,6 +31,9 @@ public class TutorialMod {
         modEventBus.addListener(this::commonSetup);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
@@ -41,7 +46,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
